c661f35d39ee1f33b75552412019d7acee6434d8
Abhinav Mingwal <abhinav.mingwal@gmail.com>
2018-09-30 19:06:17 +0530

    Update ApiServices.java

--- app/src/main/java/com/example/abhinav_pc/foodstuff/Network/ApiServices.java
+++ app/src/main/java/com/example/abhinav_pc/foodstuff/Network/ApiServices.java
@@ -11,24 +11,24 @@ import retrofit2.http.Query;
 public interface ApiServices {
 //https://developers.zomato.com/api/v2.1/search?entity_type=city&lat=28.6329&lon=77.2195&sort=rating
 
-    @Headers({"Accept: application/json","user-key: 0a12062504b77c863ace6c2f16ee7004"})
+    @Headers({"Accept: application/json","user-key: YOUR KEY HERE"})
     @GET("collections")
     Call<collectionResponce> getCollection(@Query("city_id") int city_id);
 
 
-    @Headers({"Accept: application/json","user-key: 0a12062504b77c863ace6c2f16ee7004"})
+    @Headers({"Accept: application/json","user-key: YOUR KEY HERE"})
     @GET("categories")
     Call<CategoryResponce> getCategory();
 
-    @Headers({"Accept: application/json","user-key: 0a12062504b77c863ace6c2f16ee7004"})
+    @Headers({"Accept: application/json","user-key: YOUR KEY HERE"})
     @GET("search")
     Call<RestaurantResponce> getRestraunts(@Query("entity_type") String city,@Query("lat") double lat,@Query("lon") double lon,@Query("sort") String sort);
 
-    @Headers({"Accept: application/json","user-key: 0a12062504b77c863ace6c2f16ee7004"})
+    @Headers({"Accept: application/json","user-key: YOUR KEY HERE"})
     @GET("locations")
     Call<LocationResponse> getLocation(@Query("query") String place);
 
-    @Headers({"Accept: application/json","user-key: 0a12062504b77c863ace6c2f16ee7004"})
+    @Headers({"Accept: application/json","user-key: YOUR KEY HERE"})
     @GET("restaurant")
     Call<RestaurantDetailsOnClick> getRestaurantDetails(@Query("res_id") String res_id);
 }
